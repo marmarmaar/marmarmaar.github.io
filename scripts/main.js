ac1038c79e807a4468fa8cf3a9668ef01729cb0f
marmarmaar <mar.bazueva@gmail.com>
2021-03-30 02:02:12 +0300

    mmmmmm

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,10 @@
-let html = document.documentElement;
-up.onclick = function() {
-  // при помощи JS увеличить размер шрифта html на 2px
-  html.style.fontSize = parseInt(getComputedStyle(html, '').fontSize) + 2 + 'px';
-};
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'img/me.jpg') {
+      myImage.setAttribute ('src','img/soot.png');
+    } else {
+      myImage.setAttribute ('src','img/me.jpg');
+    }
+}
